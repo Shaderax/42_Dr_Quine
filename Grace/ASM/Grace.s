56f7af38032373877c2bfb9efa293ff2d88b9e3e
Nathan ROUZEVAL <nrouzeva@e1r5p18.42.fr>
2018-09-10 15:54:47 +0200

    Save

--- Grace/ASM/Grace.s
+++ Grace/ASM/Grace.s
@@ -1,15 +1,30 @@
 %define DEF1
 %define DEF2
 %define DEF3
+extern fopen
+extern fprintf
 
 section .data
-code db ' Yo '
+code db ' %s%c%c '
+filename db 'Grace_kid.s'
+arg db 'wab+'
 
 section .text
-%macro MAIN 1
+%macro MAINE 1
+main:
+push rbp
+mov rbp, rsp
+sub rsp, 16
+lea rdi, [rel filename]
+lea rsi, [rel arg]
+call fopen
 
-
-
-
+mov rdi, rax
+lea rsi, [rel code]
+mov rdx, 0xa
+mov rcx, 0x27
+call fprintf
+leave
+ret
 %endmacro
-MAIN(1) ; Number of parameters
+MAINE 1 ; Number of parameters
