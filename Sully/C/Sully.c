735a85e27f6fea0704bd70bfdbbfaedeb52caf6f
Nathan ROUZEVAL <nrouzeva@e1r5p19.42.fr>
2018-09-12 15:24:09 +0200

    Add Sully C

--- Sully/C/Sully.c
+++ Sully/C/Sully.c
@@ -3,10 +3,16 @@
 
 int main()
 {
-	int i = 5;
-
-	if (i > 1)
-		FILE *f = fopen("Sully_", "wab+");
-	fprintf(f, );}
-	system("ls -R ../");
+int i = 5;
+char filename[512];
+char compile[512];
+char *code = "#include <stdlib.h>%1$c#include <stdio.h>%1$c%1$cint main()%1$c{%1$cint i = %4$d;%1$cchar filename[512];%1$cchar compile[512];%1$cchar *code = %2$c%3$s%2$c;%1$cif (i <= 1){return (0);};%1$csprintf(filename, %2$cSully_%%d.c%2$c, i - 1);%1$cFILE *f = fopen(filename, %2$cw%2$c);%1$cfprintf(f, code, 10, 34, code, i - 1);%1$csprintf(compile, %2$cgcc Sully_%%d.c -o Sully_%%d && ./Sully_%%d%2$c, i - 1, i - 1, i - 1);%1$cfclose(f);%1$csystem(compile);%1$creturn (0);%1$c}%1$c";
+if (i <= 1){return (0);};
+sprintf(filename, "Sully_%d.c", i - 1);
+FILE *f = fopen(filename, "w");
+fprintf(f, code, 10, 34, code, i - 1);
+sprintf(compile, "gcc Sully_%d.c -o Sully_%d && ./Sully_%d", i - 1, i - 1, i - 1);
+fclose(f);
+system(compile);
+return (0);
 }
