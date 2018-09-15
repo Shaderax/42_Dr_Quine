6aa4ae343179843e3d6b2b573fef16a180d6311d
Nathan ROUZEVAL <nrouzeva@e1r5p22.42.fr>
2018-09-15 21:24:44 +0200

    Save

--- Colleen/OCaml/Colleen.ml
+++ Colleen/OCaml/Colleen.ml
@@ -1,1 +1,11 @@
-(fun s -> Printf.printf "%s %S;;" s s) "(fun s -> Printf.printf \"%s %S;;\" s s)";; (* Oui *)
+let jesuisunefun = fun (s) -> Printf.printf s 10 s;; 
+
+(* YOLO *)
+
+let chaine = "let jesuisunefun = fun (s) -> (Printf.printf s 10 34 s);;%c%c(* YOLO *)%c%clet chaine = \"%s\";;%c%clet main = fun () -> ( (* OUi*) jesuisunefun (chaine));;%c%cmain ()";;
+
+let main = fun () -> (* OUi *) jesuisunefun ("%d%s");;
+
+(* (fun s -> Printf.printf "%s %S;;" s s ) "(fun s -> Printf.printf \"%s %S;;\" s s)" (* Oui *) *)
+
+main ()
