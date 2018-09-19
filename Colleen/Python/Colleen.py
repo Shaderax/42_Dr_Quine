8f51a2ed551c09a313f0b55dfe3e7050f410c0f5
Nathan ROUZEVAL <nrouzeva@e1r7p20.42.fr>
2018-09-19 16:46:45 +0200

    Python bonus added

---
+++ Colleen/Python/Colleen.py
@@ -0,0 +1,6 @@
+# Commentaire
+def useless():
+	# Commentary
+	return
+a = "# Commentaire 1%cdef useless():%c%c# Commentary 2%c%creturn%ca = %c%s%c%cprint a %c (10,10,9,10,9,10,34,a,34,10,37)"
+print a % (10,10,9,10,9,10,34,a,34,10,37)
