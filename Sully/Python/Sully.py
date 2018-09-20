6eb91a4aea1ba9bacce5ca06870ce70faf26ab63
Nathan ROUZEVAL <nrouzeva@e1r5p18.42.fr>
2018-09-20 22:56:41 +0200

    Save

--- Sully/Python/Sully.py
+++ Sully/Python/Sully.py
@@ -10,7 +10,7 @@ if i > 0:
     try:
         fd = open(progName,"w+")
     except IOError:
-        print 'cannot open', progName
+        print ('cannot open', progName)
         exit()
     a = "#!/usr/bin/python%cimport os%cimport sys%c%ci = %d%cif i > 0:%c    if sys.argv[0] != './Sully.py':%c        i -= 1%c    progName = %cSully_%c + str(i) + %c.py%c%c    try:%c        fd = open(progName,%cw+%c)%c    except IOError:%c        print 'cannot open', progName%c        exit()%c    a = %c%s%c%c    fd.write(a %% (10, 10, 10, 10, i, 10, 10, 10, 10, 34, 34, 34, 34, 10, 10, 34, 34, 10, 10, 10, 10, 34, a, 34, 10, 10, 10, 10, 10))%c    fd.close()%c    os.system('chmod 777 ' + progName)%c    os.system('./' + progName)%c"
     fd.write(a % (10, 10, 10, 10, i, 10, 10, 10, 10, 34, 34, 34, 34, 10, 10, 34, 34, 10, 10, 10, 10, 34, a, 34, 10, 10, 10, 10, 10))
